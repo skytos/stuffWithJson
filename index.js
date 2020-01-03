21711b2256db5c7e7e42163116b63bd5309d80b1
Skylar Broad <skylar.broad@gmail.com>
2020-01-02 19:13:10 -0800

    ditch emit and use batching

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const fs = require('fs'),
 	{ parser } = require('stream-json'),
 	{ chain } = require('stream-chain'),
-	{ streamArray } = require('stream-json/streamers/StreamArray'),
-	emit = require('stream-json/utils/emit')
+	{ streamArray } = require('stream-json/streamers/StreamArray')
+	Batch = require('stream-json/utils/Batch')
+
 
 
 const pipeline = chain([
 	fs.createReadStream('temp.json'),
 	parser(),
 	streamArray(),
-	handleItem
-]);
-emit(pipeline)
+	new Batch({batchSize: 10}),
+	handleItems
+])
 
+pipeline.on('data', () =>{})
 pipeline.on('end', () => console.log('XXXXXXXX   all done'))
 
 function delay(ms) {
 	return new Promise(f => setTimeout(f, ms))
 }
 
-async function handleItem(item) {
-	console.log('starting ' + item.value.id)
+async function handleItems(items) {
+	console.log('starting ' + items[0].value.id)
 	await delay(10)
-	console.log('finishing ' + item.value.id)
+	console.log('finishing ' + items[items.length-1].value.id)
 }

d3c7c4683ec08e3368bdcc502a54da9b1977b90a
Skylar Broad <skylar.broad@gmail.com>
2020-01-12 13:43:51 -0800

    iterator works in repl but not from cli

--- index.js
+++ index.js
@@ -4,25 +4,22 @@ const fs = require('fs'),
 	{ streamArray } = require('stream-json/streamers/StreamArray')
 	Batch = require('stream-json/utils/Batch')
 
-
-
 const pipeline = chain([
 	fs.createReadStream('temp.json'),
 	parser(),
 	streamArray(),
 	new Batch({batchSize: 10}),
-	handleItems
 ])
 
-pipeline.on('data', () =>{})
-pipeline.on('end', () => console.log('XXXXXXXX   all done'))
+const iterator = {
+	[Symbol.iterator]: function() {
+		return {
+			next: function() {
+				let value = pipeline.read();
+				return { value, done: !value } } } } }
 
-function delay(ms) {
-	return new Promise(f => setTimeout(f, ms))
+for (let chunk of iterator) {
+	console.log('XXX')
+	console.log(chunk)
 }
 
-async function handleItems(items) {
-	console.log('starting ' + items[0].value.id)
-	await delay(10)
-	console.log('finishing ' + items[items.length-1].value.id)
-}
